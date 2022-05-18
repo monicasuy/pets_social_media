9db7e3b67d8aa3540e2b1c6cd7f06333b0aea10b
monica suy <monicasuy96@gmail.com>
2022-05-18 17:34:58 +0200

    finished functionality for the search bar (which applies to the map as well)

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,14 +15,26 @@ new_user = User.create!(
     email: Faker::Internet.email,
     password: 'password'
   )
+  new_user1 = User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    address: "Assemblea de Catalunya 9-12 08911 Badalona",
+    email: Faker::Internet.email,
+    password: 'password'
+  )
   puts "User created"
 
   dog1 = Pet.create(name: "Nolan", breed: "American Staffordshire Terrier", size: "medium", sex: "male",
   description: "Nolan loves eating apples, he's super friendly and playful and loves cuddling.
   He is so calm and well behaved. So easy to love", user: User.first)
 
+  dog2 = Pet.create(name: "Pepe", breed: "American Staffordshire Terrier", size: "medium", sex: "male",
+  description: "Nolan loves eating apples, he's super friendly and playful and loves cuddling.
+  He is so calm and well behaved. So easy to love", user: User.last)
+
   puts "Nolan created"
 
   dog1.photos.attach(io: URI.open('https://res.cloudinary.com/de4v6txjq/image/upload/c_fill/v1/production/7j5sq0q38y3q5nc2zxcwp3emxgtr'), filename: 'Nolan')
+  dog2.photos.attach(io: URI.open('https://res.cloudinary.com/de4v6txjq/image/upload/c_fill/v1/production/zyieb3860e5tpmwn4yhe1dg8nxfq'), filename: 'Nolan')
 
   puts 'Photo attached to Nolan'
